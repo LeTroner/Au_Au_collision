3da10244140a8bb5656e559327265f111e8469cc
Barnabas <barnabas.porfy@gmail.com>
2017-03-05 23:50:44 +0100

    Problem
    Found the problem, its that the txt not only consists numbers but string also, thats why i get an error. Will solve it soon

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ AuAu_dict = {
 AuAu_list_dict = {}
 AuAu_list = []
 
-with open("AuAu_big_cut.txt") as f:
-    for line in itertools.islice(f, 1, None):
+with open("AuAu_cut.txt") as f:
+    for lines in itertools.islice(f, 1, None):
         for line in f:
             cur_part = ''                                                       # resets current particle
             for i in range(20, 26):                                             # runs thru on indexes 20-26
                 cur_part += line[i]                                             # the line i-th char puts into cur_part
-                cur_part = cur_part.strip()                                     # removing whitespace\
-            int(cur_part)                                                       # conversion to int
-            AuAu_list.append(cur_part)                                          # adding every read cur_part to the list
+                cur_part = cur_part.strip()                                     # removing whitespace
+            print(cur_part)
+            AuAu_list.append(int(cur_part))                                          # adding every read cur_part to the list
         for c in AuAu_list:                                                     # looping thru the particles in the same list
             #if c not in AuAu_dict:
             #    print("Current particle are not in the dictionary ")
