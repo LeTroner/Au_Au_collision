1a308894c6c811f9efd6dfa9eac353edd5f89a05
Unknown <220mph@gmail.com>
2017-04-11 15:07:28 +0200

    Average counting
    In this update I added average counting, pretty easy configuration.

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from processes import *
 
 #----------MAIN ---------
 
-countPart("AuAu_cut.txt")
+event = countPart("AuAu_1000_b_09_10\.txt")
+average = avPart(event,AuAu_list_dict)
--- processes.py
+++ processes.py
@@ -25,38 +25,22 @@ def countPart(file):
             if particle not in AuAu_list_dict:
                 AuAu_list_dict[particle] = 1
             AuAu_list_dict[particle] += 1
-        print(AuAu_list_dict)
-        print(event)
-        print(" number of events occured")
+        #print(AuAu_list_dict)
+        print(str(event)+ " number of events occured")
+        return event
 
-
-'''        
-            for line in f:
-                    # resets current particle
-                    cur_part = ''
-                    # runs thru on indexes 20-26 because there are
-                    #the particle ID
-                    for i in range(20, 26):
-                        try:
-                            # the line i-th char puts into cur_part
-                            cur_part += line[i]
-                            # removing whitespace
-                            cur_part = cur_part.strip()
-                        except (IndexError):
-                            pass
-                    # adding every read cur_part to the list
-                    AuAu_list.append(int(cur_part))
-                # looping thru the particles in the same list
-            for c in AuAu_list:
-                #if c not in AuAu_dict:
-                #    print("Current particle are not in the dictionary ")
-                #else:
-                    # checking if the dict has already the key or not
-                    if c not in AuAu_list_dict:
-                        AuAu_list_dict[c] = 1
-                    else:
-                        AuAu_list_dict[c] += 1
-    #            for p in AuAu_list:
-    #                AuAu_list_dict[p] = AuAu_dict[p]
-        print(AuAu_list_dict)
-    # print(AuAu_list)'''
+#average particle per event
+def avPart(event,part_dict):
+#making event to float in order to get
+#exact number
+#declaring average dict where we store the particle ID and its average per event
+    event = float(event)
+    average_dict = {}
+#looping in the elements of part_dict
+    for i in part_dict:
+#getting the average particle per event
+        average_particle = part_dict.get(i)/event
+        average_dict[i] = average_particle
+    print("On average in %d"  %int(event) + " event(s) we have: ")
+    print(average_dict)
+    return average_dict
