446f32250a610a965e74147f425e870a49c83165
Unknown <220mph@gmail.com>
2017-04-12 20:01:26 +0200

    Reading only one line
    Lowering the memory need
    Current problem is random error 1 which is appears when the next line is BEGINNINGOFEVENT, and the current lines first element, which is the number of particle is divided by the numberofParticle variable. This happens several times.

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from processes import *
 
 #----------MAIN ---------
 
-event = countPart("AuAu_1000_b_09_10\.txt")
+event = countPart("AuAu_1000_b_09_10.txt")
 average = avPart(event,AuAu_list_dict)
--- processes.py
+++ processes.py
@@ -4,30 +4,50 @@ import itertools
 
 #defining a particle counter
 def countPart(file):
+    #declaring variables which will be used to store info
     particle_list = []
     particle = 0
-    event = 1
+    event = 0
+    numberofParticle = 0
     with open(file) as f:
-        lines = f.readlines()[28:]
-        #reading thru the lines
-        for line in range(len(lines)):
+        #reading thru the lines and skipping the first 28 line
+        for line in itertools.islice(f, 28, None):
             try:
-                #on every line the particle id is on the same
-                #spot, with this method, we only get the ID
-                particle = int(lines[line].split()[1])
-                particle_list.append(particle)
-                #if the current particle is not in the
-                #AuAu_list_dict yet, assign 1 to its
-                #value
-            except IndexError:
-                line += 2
+                nextLine = next(f).split()
+            except StopIteration:
+                print(" The file contains no more lines ")
+
+            split_line = line.split()
+            #if the current line first word is BEGINNINOFEVENT
+            #then add 1 to the event counter and check if we got the same
+            #amount of particle as it was in the line
+            if nextLine[0] == 'BEGINNINGOFEVENT':
+                line_0 = int(split_line[0])
+                if line_0/numberofParticle != 1:
+                    print(" Error 1")
+                continue
+            if split_line[0] == 'BEGINNINGOFEVENT':
+                #setting the current event's numbers
+                numberofParticle = 0
                 event += 1
+                #splitting the very after of the beginningofevent line
+                #the second item of the line is #of the particles
+                numberofParticle = int(nextLine[1])
+                nextLine = 0
+                try:
+                    split_line = f.next().split()
+                except StopIteration:
+                    pass
+            #splitting the line because above we had already the line
+            #we need
+            particle = int(split_line[1])
             if particle not in AuAu_list_dict:
                 AuAu_list_dict[particle] = 1
-            AuAu_list_dict[particle] += 1
-        #print(AuAu_list_dict)
-        print(str(event)+ " number of events occured")
-        return event
+            else:
+                AuAu_list_dict[particle] += 1
+    return event
+
+
 
 #average particle per event
 def avPart(event,part_dict):
