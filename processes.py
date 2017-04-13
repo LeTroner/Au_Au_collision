12f1df60e877f87ad5a468a3052898ff9bbe9b12
Unknown <220mph@gmail.com>
2017-04-13 14:48:18 +0200

    Solved the problem
    No longer gets the error 1 when looping through which means everything works

--- processes.py
+++ processes.py
@@ -12,37 +12,33 @@ def countPart(file):
     with open(file) as f:
         #reading thru the lines and skipping the first 28 line
         for line in itertools.islice(f, 28, None):
-            try:
-                nextLine = next(f).split()
-            except StopIteration:
-                print(" The file contains no more lines ")
-
             split_line = line.split()
-            #if the current line first word is BEGINNINOFEVENT
-            #then add 1 to the event counter and check if we got the same
-            #amount of particle as it was in the line
-            if nextLine[0] == 'BEGINNINGOFEVENT':
-                line_0 = int(split_line[0])
-                if line_0/numberofParticle != 1:
-                    print(" Error 1")
-                continue
             if split_line[0] == 'BEGINNINGOFEVENT':
-                #setting the current event's numbers
-                numberofParticle = 0
                 event += 1
                 #splitting the very after of the beginningofevent line
                 #the second item of the line is #of the particles
-                numberofParticle = int(nextLine[1])
-                nextLine = 0
-                try:
-                    split_line = f.next().split()
-                except StopIteration:
-                    pass
-            #splitting the line because above we had already the line
-            #we need
+                numberofParticle = int(f.next().split()[1])
+                # redeclaring because above we were using the next line to skip
+                split_line = f.next().split()
+
+            #if the current line's second item
+            #is the same as the numberofParticle
+            #then we reached the end of the current event
+            try:
+                split_line[1] == numberofParticle
+                #if the statement is true, continue
+            except IndexError:
+                #else, there is an error with the amount of
+                #particles said and been
+                print(" Error 1")
+            #the current particle ID is the second item
+            #of the split_line
             particle = int(split_line[1])
+            #if its not in the dict yet
+            #set its value to 1
             if particle not in AuAu_list_dict:
                 AuAu_list_dict[particle] = 1
+            #else, increase its value by one
             else:
                 AuAu_list_dict[particle] += 1
     return event
